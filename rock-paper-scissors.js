a9a8357204e14a226f7e4851100eb5fca41ba552
Jaime17382 <145494311+jaime17382@users.noreply.github.com>
2024-01-25 21:44:26 -0600

    prompt only taking rock, paper, or scissors now. Game works fine. Have to add rounds and win counter.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,26 +1,52 @@
-/*function playGame() {
+function playGame() {
     // Array of possible moves
-    const moves = ["rock", "paper", "scissors"]; */
+    let moves = ["rock", "paper", "scissors"]; 
+
+     // Computer randomly selects a move
+     let computerChoice = moves[Math.floor(Math.random() * moves.length)];
+     
     let choiceOne = "rock";
     let choiceTwo = "paper";
     let choiceThree = "scissors";
 
     /*
     my problem is-
-    when checking if prompt input is rock 
+    when checking if prompt input is rock paper or scissors, 
+    the check also allows any character within the strings rock paper or scissors,
+    how can i make sure prompt ONLY accepts the entire string 
     */
 
     // Get user's choice
     let userChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
 
-    if (userChoice === choiceOne || userChoice === choiceTwo || userChoice === choiceThree ) {
-        console.log(true);
+      if (userChoice === choiceOne || userChoice === choiceTwo || userChoice === choiceThree ) {
+        console.log();
       }
       else {
         console.log("Please enter Rock, Paper, or Scissors")
       }
 
-   
+
+     // Display choices
+     console.log("You chose: " + userChoice);
+     console.log("Computer chose: " + computerChoice);
+ 
+     // Determine the winner
+     if (userChoice === computerChoice) {
+         console.log("It's a tie!");
+     } else if (
+         (userChoice === "rock" && computerChoice === "scissors") ||
+         (userChoice === "paper" && computerChoice === "rock") ||
+         (userChoice === "scissors" && computerChoice === "paper")
+     ) {
+         console.log("You win!");
+     } else {
+         console.log("Computer wins!");
+     }
+    }
+ 
+ // Call the function to play the game
+ playGame();
 
   
 
@@ -31,8 +57,6 @@
 
 
 /*
-
- 
 
 let pattern = /^["Rock", "Paper", "Scissors"]/;
 
